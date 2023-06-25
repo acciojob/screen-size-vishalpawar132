f94131cce6f5013ee95cfb32c7019dcb2f45745d
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-06-25 17:24:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+ function updateSize() {
+      // Get the window width and height
+      var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+      
+      // Get the sizeText element
+      var sizeText = document.getElementById('sizeText');
+      
+      // Update the size information
+      sizeText.textContent = 'Width: ' + width + 'px, Height: ' + height + 'px';
+    }
+
+    // Call the updateSize function on window resize
+    window.addEventListener('resize', updateSize);
+
+    // Initial call to updateSize to display the initial window size
+    updateSize();
